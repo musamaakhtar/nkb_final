59032217d63011f2a2006f727dab46cf7acc6856
instaHippo <instahippo26@gmail.com>
2024-05-06 20:59:12 +0500

    envchanges

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-
+require('dotenv').config();
 const  mongoURI = "mongodb+srv://bhattacharjeesolution:bhattacharjeesolution@cluster0.otfdv7w.mongodb.net/nkb";
 // const  mongoURI = "mongodb://localhost:27017/nkb";
 
@@ -10,7 +10,7 @@ mongoose.set("strictQuery", false);
 
 const connectToMongo = async ()=>{
     try {
-        await mongoose.connect(mongoURI);
+        await mongoose.connect(process.env.mongoURI || mongoURI);
         console.log("connected to mongo successfully")
     } catch (error) {
         console.log(error);
