69c780cf8160cc528805d8115d805021b7e2e60c
instaHippo <instahippo26@gmail.com>
2024-05-06 21:09:14 +0500

    newxhange

--- db.js
+++ db.js
@@ -1,16 +1,19 @@
 const mongoose = require("mongoose");
 
+const { MongoClient } = require('mongodb');
+
 require('dotenv').config();
-const  mongoURI = "mongodb+srv://bhattacharjeesolution:bhattacharjeesolution@cluster0.otfdv7w.mongodb.net/nkb";
+const  mongoURI = process.env.mongoURI || "mongodb+srv://bhattacharjeesolution:bhattacharjeesolution@cluster0.otfdv7w.mongodb.net/nkb";
 // const  mongoURI = "mongodb://localhost:27017/nkb";
 
+const client = new MongoClient(mongoURI);
 
 
 mongoose.set("strictQuery", false);
 
 const connectToMongo = async ()=>{
     try {
-        await mongoose.connect(process.env.mongoURI || mongoURI);
+        await client.connect(process.env.mongoURI || mongoURI);
         console.log("connected to mongo successfully")
     } catch (error) {
         console.log(error);
