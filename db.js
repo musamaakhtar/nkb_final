ef93b07a9f8f0dc317c73e4a7aef5dfbfa449ce8
instaHippo <instahippo26@gmail.com>
2024-05-12 00:24:26 +0500

    final

--- db.js
+++ db.js
@@ -1,21 +1,49 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
+const { MongoClient } = require('mongodb');
 
 
 require('dotenv').config();
 const  mongoURI = "mongodb+srv://bhattacharjeesolution:bhattacharjeesolution@cluster0.otfdv7w.mongodb.net/nkb";
 // const  mongoURI = "mongodb://localhost:27017/nkb";
 
+const connectOptions = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+};
+// let dbClient = null;
 
-
-mongoose.set("strictQuery", false);
-
-const connectToMongo = async ()=>{
-    try {
-        await mongoose.connect(process.env.mongoURI || mongoURI);
-        console.log("connected to mongo successfully")
-    } catch (error) {
-        console.log(error);
-    }
+function connectToMongoDB() {
+    MongoClient.connect(mongoURI, connectOptions)
+        .then(client => {
+            console.log('Connected to MongoDB');
+            // dbClient = client;
+            // Handle any required database initialization or setup here
+        })
+        .catch(err => {
+            console.error('Error connecting to MongoDB:', err.message);
+        });
 }
 
-module.exports= connectToMongo ;
+// Function to periodically reconnect
+// function startReconnectInterval() {
+//     setInterval(() => {
+//         if (!dbClient || !dbClient.isConnected()) {
+//             console.log('Attempting to reconnect to MongoDB...');
+//             connectToMongoDB();
+//         }
+//     }, 5000); // Reconnect every 5 seconds (adjust as needed)
+// }
+
+
+// mongoose.set("strictQuery", false);
+
+// const connectToMongo = async ()=>{
+//     try {
+//         await mongoose.connect(process.env.mongoURI || mongoURI);
+//         console.log("connected to mongo successfully")
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+// startReconnectInterval();
+module.exports= connectToMongoDB ;
--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-const connectToMongo = require("./db");
+const connectToMongoDB = require("./db");
 var cors = require("cors");
 // var bodyParser = require('body-parser')
-
-
-connectToMongo();
-
+connectToMongoDB();
 const express = require('express')
 const app = express()
-const port = 5005
+const port = 80
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
-// app.use(bodyParser.urlencoded({ extended: false }))  // for form-encode
+// const corsOptions = {
+//   origin: '*', // Allow only requests from this origin
+//   methods: ['GET', 'POST' , 'PUT' , 'REMOVE' , 'DELETE' , 'PATCH' , 'FETCH'],      // Allow only specified HTTP methods
+//   allowedHeaders: ['Content-Type'], // Allow only specified headers
+// };
 app.use(cors());
+// app.use(bodyParser.urlencoded({ extended: false }))  // for form-encode
+// app.use(cors());
 // to use request.body we need to use below middileware
 app.use(express.json());
 app.use("/static",express.static("static") )
-
 // available routes
 app.use("/api/user",require('./routes/UserRoute'));
 app.use("/api/super-admin", require("./routes/SuperAdminRoute"));
