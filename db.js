d03b288a61c18407de2d0ec8e1673d03def5c248
Muhammad Usama <112926977+musamaakhtar@users.noreply.github.com>
2024-05-12 00:46:09 +0500

    mongoURI

--- db.js
+++ db.js
@@ -1,60 +1,21 @@
-// const mongoose = require("mongoose");
-const { MongoClient } = require('mongodb');
+const mongoose = require("mongoose");
 
 
 require('dotenv').config();
 const  mongoURI = "mongodb+srv://bhattacharjeesolution:bhattacharjeesolution@cluster0.otfdv7w.mongodb.net/nkb";
 // const  mongoURI = "mongodb://localhost:27017/nkb";
 
-const connectOptions = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-};
-// let dbClient = null;
 
-async function connectToMongoDB() {
-   try {
-        const client = new MongoClient(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
-        await client.connect();
-        console.log('Connected to MongoDB');
-        // dbClient = client;
-        // Handle any required database initialization or setup here
+
+mongoose.set("strictQuery", false);
+
+const connectToMongo = async ()=>{
+    try {
+        await mongoose.connect(process.env.mongoURI || mongoURI);
+        console.log("connected to mongo successfully")
     } catch (error) {
-        console.error('Error connecting to MongoDB:', error.message);
+        console.log(error);
     }
-    //  const client = new MongoClient(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
-    //  await client.connect()
-    //     .then(client => {
-    //         console.log('Connected to MongoDB');
-    // // MongoClient.connect(mongoURI, connectOptions)
-    //         // dbClient = client;
-    //         // Handle any required database initialization or setup here
-    //     })
-    //     .catch(err => {
-    //         console.error('Error connecting to MongoDB:', err.message);
-    //     });
 }
 
-// Function to periodically reconnect
-// function startReconnectInterval() {
-//     setInterval(() => {
-//         if (!dbClient || !dbClient.isConnected()) {
-//             console.log('Attempting to reconnect to MongoDB...');
-//             connectToMongoDB();
-//         }
-//     }, 5000); // Reconnect every 5 seconds (adjust as needed)
-// }
-
-
-// mongoose.set("strictQuery", false);
-
-// const connectToMongo = async ()=>{
-//     try {
-//         await mongoose.connect(process.env.mongoURI || mongoURI);
-//         console.log("connected to mongo successfully")
-//     } catch (error) {
-//         console.log(error);
-//     }
-// }
-// startReconnectInterval();
-module.exports= connectToMongoDB ;
+module.exports= connectToMongo ;
